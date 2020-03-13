eaf4029f3362e744b92e3c6478c493d355074842
Stéphane PATERNA <paterna@club-internet.fr>
2020-03-13 11:58:28 +0100

    Ajout des fichiers index.html et ex01.js

---
+++ ex01.js
@@ -0,0 +1,14 @@
+var number=2
+console.log (typeof number);
+
+var string = 'Salut ma poule';
+console.log (typeof string);
+
+var tab=['a','b','c','d','e'];
+console.log (typeof tab);
+
+var aBoolean = false;
+console.log(typeof aBoolean);
+
+var car = {type:"Fiat", model:"500", color:"white"};
+console.log(typeof car);
