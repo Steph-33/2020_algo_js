578ac0ed914c6d24246d46bfbfcc4d20f7f09fe2
Stéphane PATERNA <paterna@club-internet.fr>
2020-03-14 17:23:50 +0100

    Mise à jour de index.html et ajout de ex06.js

---
+++ ex06.js
@@ -0,0 +1,16 @@
+// Faire une fonction qui retourne true. 
+function returnTrue(){
+    return true;
+}
+
+// Faire une fonction qui prend en paramètre une chaîne de caractères et 
+// qui retourne cette même  chaîne.
+function string(a){
+    return a;
+} 
+
+// Faire une fonction qui prend en paramètre deux chaînes de caractères et 
+// qui revoit la concaténation de ces deux chaînes
+function concat(a,b){
+    return a+b;
+}
