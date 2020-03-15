aa793d46089d3f6cc6c6676d060b48696636b45e
Stéphane PATERNA <paterna@club-internet.fr>
2020-03-15 12:59:05 +0100

    Modification de ex06.js

--- ex06.js
+++ ex06.js
@@ -14,3 +14,19 @@ function string(a){
 function concat(a,b){
     return a+b;
 }
+
+// Faire une fonction qui prend en paramètre deux nombres. 
+// La fonction doit retourner : 
+// 'Le premier nombre est plus grand' si le premier nombre est plus grand que le deuxième 
+// 'Le premier nombre est plus petit' si le premier nombre est plus petit que le deuxième 
+// 'Les deux nombres sont identiques' si les deux nombres sont égaux
+function number(a,b){
+    if(a>b){
+        return ('Le premier nombre est plus grand');
+    }else if(a<b){
+        return ('Le premier nombre est plus petit');
+    }
+    else{
+        return ('Les deux nombres sont identiques');
+    }
+}
