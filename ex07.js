1ee67c7d4f05a3a2e235049cfde6a810d321f16c
Stéphane PATERNA <paterna@club-internet.fr>
2020-03-23 18:31:54 +0100

    Ajout ex07.js

---
+++ ex07.js
@@ -0,0 +1,9 @@
+//Création d'une boucle for pour itérer sur toutes les valeurs du tableau "prenoms".
+//1. Supprimer les doublons du tableau "valeurs"
+//2. Faite une concaténation du tableau "valeurs" en chaîne de caractères avec le séparateur suivant ", "
+//3. Afficher le résultat dans la console!
+//const prenoms = ["Thomas", "Sarah", "Alexandre","Alexandre",, "Mélodie", "Jonathan", "Laure"];
+
+
+
+
